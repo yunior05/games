abec22719f7d6368df4c72c414985152dd6f776d
yun0509 <yun0509av@bitbucket.org>
2018-08-22 15:29:33 -0400

    Change gameplay to trailer
    Change the variable gameplay_youtube to trailer_youtube and related

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import webbrowser
 
 class Games():
 
-	def __init__(self, title_game, year_game, genre_game, gameplay_youtube):
+	def __init__(self, title_game, year_game, genre_game, trailer_youtube):
 		self.title = title
 		self.year = year_game
 		self.genre = genre_game
-		self.gameplay = gameplay_youtube
+		self.trailer = trailer_youtube
 
 	def show_gameplay(self):
-		webbrowser.open(self.gameplay)
-		
+		webbrowser.open(self.trailer)
+
