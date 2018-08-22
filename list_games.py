e7678eb9449785c21deb2947fe3bfa231baa5d61
yun0509 <yun0509av@bitbucket.org>
2018-08-22 15:59:48 -0400

    Add fresh_tomatoes.py
    Add frsh_tomatoes.py, this file create a web page with the games

--- list_games.py
+++ list_games.py
@@ -3,20 +3,23 @@ import main
 League_of_Legends = main.Games("League of Legends",
 								"27/10/2009",
 								"Moba",
+								"https://es.wikipedia.org/wiki/League_of_Legends#/media/File:League_of_Legends.png",
 								"https://www.youtube.com/watch?v=vzHrjOMfHPY") 
 
 Legend_of_Zelda = main.Games("The Legend of Zelda: Ocarina of Time",
 								"21/11/1998",
 								"Action-Adventure",
+								"https://es.wikipedia.org/wiki/The_Legend_of_Zelda:_Ocarina_of_Time#/media/File:The_Legend_of_Zelda_Ocarina_of_Time.svg",
 								"https://www.youtube.com/watch?v=pF7p9hruSeY")
 
 Fortnite = main.Games("Fortnite",
 						"21/07/2017",
 						"Battle Royale",
+						"https://es.wikipedia.org/wiki/Fortnite_(videojuego)#/media/File:Fortnite.png",
 						"https://www.youtube.com/watch?v=2gUtfBmw86Y")
 
 Portal = main.Games("Portal",
 						"9/10/2007",
 						"Puzzle, Logic",
+						"https://es.wikipedia.org/wiki/Portal_(videojuego)#/media/File:Portal_logo.png",
 						"https://www.youtube.com/watch?v=TluRVBhmf8w")
-
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import webbrowser
 
 class Games():
 
-	def __init__(self, title_game, year_game, genre_game, trailer_youtube):
+	def __init__(self, title_game, year_game, genre_game,poster_image, trailer_youtube):
 		self.title = title_game
 		self.year = year_game
 		self.genre = genre_game
+		self.poster = poster_image
 		self.trailer = trailer_youtube
 
 	def show_gameplay(self):
