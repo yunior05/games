c0dfe104d51ae94f031a486597ea689eba18ddbc
yun0509 <yun0509av@bitbucket.org>
2018-08-22 15:40:58 -0400

    Add a file list_games
    Create a file list_games whit the list of my favorite games

---
+++ list_games.py
@@ -0,0 +1,22 @@
+import main
+
+League_of_Legends = main.Games("League of Legends",
+								"27/10/2009",
+								"Moba",
+								"https://www.youtube.com/watch?v=vzHrjOMfHPY") 
+
+Legend_of_Zelda = main.Games("The Legend of Zelda: Ocarina of Time",
+								"21/11/1998",
+								"Action-Adventure",
+								"https://www.youtube.com/watch?v=pF7p9hruSeY")
+
+Fortnite = main.Games("Fortnite",
+						"21/07/2017",
+						"Battle Royale",
+						"https://www.youtube.com/watch?v=2gUtfBmw86Y")
+
+Portal = main.Games("Portal",
+						"9/10/2007",
+						"Puzzle, Logic",
+						"https://www.youtube.com/watch?v=TluRVBhmf8w")
+
