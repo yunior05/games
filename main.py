a231d806fc3b6455f47511d8c1e838795e49c722
yun0509 <yun0509av@bitbucket.org>
2018-08-22 15:43:40 -0400

    Change variable title
    Change variable title to title_game

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webbrowser
 class Games():
 
 	def __init__(self, title_game, year_game, genre_game, trailer_youtube):
-		self.title = title
+		self.title = title_game
 		self.year = year_game
 		self.genre = genre_game
 		self.trailer = trailer_youtube
